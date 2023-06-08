1ab2a2d66cfdbaf68479463dc5ccd2d281bb4b42
AntonShiva <ryashinanthon@yandex.ru>
2023-06-08 18:18:21 +0300

    code 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,26 +1,108 @@
 //
 //  ContentView.swift
-//  Memorize
+//  Memory_Training_CS193P
 //
-//  Created by Anton Rasen on 08.06.2023.
+//  Created by Anton Rasen on 06.06.2023.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    var emojis = ["🛶", "🛳️", "🚀", "🚘", "🚁",  "🛺", "🛸", "✈️", "🚜", "🚎", "🚲", "🛰️", "🛴", "🚚", "🚤", "🛵", "🚕", "🚃", "⛵️", "🏍️", "🛻", "🚂", "🏎️", "🚢", ]
+  @State  var emojiCount = 20
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+          ScrollView {
+              LazyVGrid (columns: [GridItem(.adaptive(minimum: 65))]) {
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji).aspectRatio(2/3,  contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+            remove
+            Spacer()
+           add
         }
-        .padding()
+            .font(.largeTitle)
+            .padding(.horizontal)
     }
-}
+            .padding(.horizontal)
+            
+    }
+     
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+                   }
+                 } label: {
+                    Image(systemName: "minus.circle")
+            }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
-}
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+                     }
+                    } label: {
+                      Image(systemName: "plus.circle")
+            }
+        }
+    
+    }
+
+
+    struct CardView: View {
+        var content: String
+      @State  var isFacUp: Bool = true
+        
+        var body: some View {
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: 20.0)
+                   if isFacUp {
+                    shape.fill().foregroundColor(.white)
+                    shape.strokeBorder(lineWidth: 3)
+                    Text(content).font(.largeTitle)
+                }  else {
+                    shape.fill()
+                }
+            }
+            
+            .onTapGesture {
+                isFacUp = !isFacUp
+            }
+            
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+                .previewDevice("iPhone 14 Pro")
+        }
+    }
+
+
